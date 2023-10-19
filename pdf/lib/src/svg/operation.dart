a2c875e227cfd90d891aff7c19196794ffa71bbc
Erwan Martin <hello@erwan.io>
2023-10-19 11:33:36 +0200

    update log message

--- pdf/lib/src/svg/operation.dart
+++ pdf/lib/src/svg/operation.dart
@@ -71,7 +71,7 @@ abstract class SvgOperation {
           return SvgUse.fromXml(element, painter, brush);
       }
     } catch (e) {
-      print('[Error]<Error parsing SVG element: ${e}>');
+      print('[pdf][SvgOperation] Error parsing SVG element: ${e}');
       return null;
     }
 
