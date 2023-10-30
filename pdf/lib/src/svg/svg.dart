0f61c7147c017a8f80a9e7e6247d7735aa24bdac
Erwan Martin <hello@erwan.io>
2023-10-30 18:11:56 +0100

    update scale

--- pdf/lib/src/svg/svg.dart
+++ pdf/lib/src/svg/svg.dart
@@ -202,7 +202,7 @@ class EmbeddedSvg extends SvgOperation {
     canvas
       ..saveContext()
       ..setTransform(Matrix4.identity()
-        ..scale(sx, -sy)
+        ..scale(sx, sy, 1)
         ..translate(x, y));
 
     for (final child in children) {
