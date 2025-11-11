14e7492933d60aa02042e5430715760331edf785
Erwan Martin <hello@erwan.io>
2025-11-11 14:48:51 +0100

    Add space after /Span BDC for proper PDF syntax

--- pdf/lib/src/pdf/graphics.dart
+++ pdf/lib/src/pdf/graphics.dart
@@ -546,7 +546,7 @@ class PdfGraphics {
       return true;
     }());
 
-    _buf.putString('/Span BDC');
+    _buf.putString('/Span BDC ');
     _buf.putString('[');
     font.putText(_buf, s);
     _buf.putString(']TJ ');
@@ -595,7 +595,7 @@ class PdfGraphics {
     PdfNumList([x, y]).output(_page, _buf);
     _buf.putString(' Td ');
 
-    _buf.putString('/Span BDC');
+    _buf.putString('/Span BDC ');
     _buf.putString('[');
     font.putGlyphs(_buf, glyphIndices);
     _buf.putString(']TJ ');
